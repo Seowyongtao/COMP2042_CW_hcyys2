2fede7e7e9f97c87cbd6d65a6f6eec40f1295c63
Seow Yong Tao <seowyongtao22@gmail.com>
2021-11-30 21:05:15 +0800

    Refactor HomeMenu to follow MVC pattern: Setup HomeMenuController and create logic for action performed by start button and exit button.
    This is my fourth step to refactor HomeMenu to follow the MVC design pattern.

--- src/test/HomeMenuController.java
+++ src/test/HomeMenuController.java
@@ -1,4 +1,38 @@
 package test;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class HomeMenuController {
+
+    private HomeMenuView theView;
+    private GameFrame owner;
+
+    public HomeMenuController(HomeMenuView theView, GameFrame owner){
+
+        this.theView = theView;
+        this.owner = owner;
+
+        this.theView.addExitListener(new ExitListener());
+        this.theView.addStartListener(new StartListener());
+
+    }
+
+    class ExitListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Goodbye " + System.getProperty("user.name"));
+            System.exit(0);
+        }
+
+    }
+
+    class StartListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Start Game");
+            owner.enableGameBoard();
+        }
+
+    }
 }
