0c7fb10eda5983e3d55c8b2e527ca182dbe63c95
Seow Yong Tao <seowyongtao22@gmail.com>
2021-12-09 08:19:33 +0800

    Junit tests: Create BlockTest class for creating Junit tests for Block class

---
+++ Test/test/BlockTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlockTest {
+
+    Block block;
+
+    @BeforeEach
+    public void setup(){
+        block = new Block(120, 190, 80,10);;
+    }
+
+    @Test
+    void setInnerColorTest() {
+        block.setInnerColor(Color.red);
+        assertEquals(block.getInnerColor(), Color.red);
+
+        block.setInnerColor(Color.green);
+        assertEquals(block.getInnerColor(), Color.green);
+    }
+
+    @Test
+    void setBorderColorTest() {
+        block.setBorderColor(Color.red);
+        assertEquals(block.getBorderColor(), Color.red);
+
+        block.setBorderColor(Color.green);
+        assertEquals(block.getBorderColor(), Color.green);
+    }
+
+    @Test
+    void impactTest() {
+        Ball ball = new RubberBall(new Point(120,190));
+        block.setVisible();
+        assertTrue(block.impact(ball));
+    }
+
+}
